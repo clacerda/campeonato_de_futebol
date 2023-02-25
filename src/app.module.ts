5f4563fae1042009ec168f8ac5cc71d964d0beaf
clacerda <claudiovieira_jr@hotmail.com>
2023-02-25 17:36:56 -0300

    Inclusão de controllers de jogadores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { JogaresModule } from './jogares/jogares.module';
+import { Module } from '@nestjs/common'; 
+import { JogadoresController } from './jogadores/jogadores.controller';
+import { JogadoresModule } from './jogadores/jogadores.module';
 
 @Module({
-  imports: [JogaresModule],
+  imports: [JogadoresModule, JogadoresModule],
+  controllers: [JogadoresController],
+  providers: [],
 })
 export class AppModule {}
