bca7442acaf0cd3145cc4fc71fbfef17cd3471d7
clacerda <claudiovieira_jr@hotmail.com>
2023-02-27 19:46:04 -0300

    Método criar jogador/ instalação de lib uuid

---
+++ src/jogadores/jogadores.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+import { Jogador } from './interfaces/jogador.interface';
+import {v4 as uuidv4} from 'uuid';
+
+@Injectable()
+export class JogadoresService {
+
+    private jogadores: Jogador[] = [];
+    private readonly logger = new Logger(JogadoresService.name);
+
+    async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void>{
+
+      this.logger.log(`Cria jogadorDTO: ${criaJogadorDto}`);
+      await this.criar(criaJogadorDto);
+
+    }
+
+    private criar(criaJogadorDto: CriarJogadorDto): void {
+
+      const { nome, telefoneCelular, email} = criaJogadorDto;
+      
+      const jogador: Jogador = {
+        _id: uuidv4(),
+        nome,
+        telefoneCelular,
+        email,
+        ranking: 'A',
+        posicaoRanking: 1,
+        urlFotoJogador: 'www.google.com.br/foto'
+      };
+
+      this.jogadores.push(jogador);
+
+    }
+    
+}
