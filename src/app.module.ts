a71c5891ea0695325bd8cbdfeab3ccca8fcda55a
clacerda <claudiovieira_jr@hotmail.com>
2023-03-09 20:12:33 -0300

    CONNECTION MONGODB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JogadoresController } from './jogadores/jogadores.controller';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:CIWtDXQkmhp1oGBm@cluster0.bwloubc.mongodb.net/smartTraking?retryWrites=true&w=majority', 
+    { useNewUrlParser: true, 
+
+      useUnifiedTopology: true  }),
+    JogadoresModule],
   controllers: [JogadoresController],
 })
 export class AppModule {}
