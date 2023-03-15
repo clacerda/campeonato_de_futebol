49e35fb57c5128b8248b6475970872640bbd9faf
clacerda <claudiovieira_jr@hotmail.com>
2023-03-15 19:56:56 -0300

    Persistencia - categoria

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
+import { CategoriasService } from './categorias.service';
 
-@Controller('categorias')
-export class CategoriasController {}
+@Controller('api/v1/categorias')
+export class CategoriasController {
+
+    constructor(private readonly categoriaService: CategoriasService){}
+
+    @Post()
+    @UsePipes(ValidationPipe)
+    async criarCategoria(
+      @Body() criarCategoriaDto: CriarCategoriaDto): Promise<Categoria>{
+
+        return await this.categoriaService.criarCategoria(criarCategoriaDto);
+
+      }
+
+}
